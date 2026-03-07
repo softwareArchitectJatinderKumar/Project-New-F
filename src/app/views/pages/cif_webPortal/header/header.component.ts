c76b97db8e6be93fe2ec4fa018ce1f395fcb9af8
Jatinder kumars <jatinderkumar.softwaredeveloper@gmail.com>
2026-03-07 10:57:38 +0530

    Fixed Events Slider on all screen

--- src/app/views/pages/cif_webPortal/header/header.component.ts
+++ src/app/views/pages/cif_webPortal/header/header.component.ts
@@ -1,81 +1,61 @@
-import { Component, Inject, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { DOCUMENT } from '@angular/common';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-
-// Declare jQuery if the header uses it for hover effects
-declare var $: any;
+import { catchError, finalize } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+   selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+
+  encapsulation: ViewEncapsulation.None 
 })
-export class HeaderComponent implements OnInit, AfterViewInit {
-  headerHtml: SafeHtml = '';
+export class HeaderComponent implements OnInit {
+  headerHtml: SafeHtml | null = null;
+  loading: boolean = true;
+  error: boolean = false;
 
   constructor(
-    private http: HttpClient,
-    private sanitizer: DomSanitizer,
-    @Inject(DOCUMENT) private document: Document
+    private http: HttpClient, 
+    private sanitizer: DomSanitizer
   ) {}
 
   ngOnInit(): void {
-    this.http.get('/php-header', { responseType: 'text' }).subscribe({
-      next: (html: string) => {
-        this.headerHtml = this.sanitizer.bypassSecurityTrustHtml(html);
-        
-        // Wait a tiny bit for Angular to render the HTML into the view
-        setTimeout(() => {
-          this.reinitializeHeaderScripts();
-        }, 100);
-      },
-      error: (err) => console.error('Error:', err)
+    this.fetchHeader();
+  }
+
+  fetchHeader(): void {
+   // Ensure this matches the key in proxy.conf.json EXACTLY
+  this.http.get('/php-header', { responseType: 'text' })
+    .pipe(
+      catchError((err) => {
+        console.error('Proxy Error Details:', err); // Log the actual error to the console
+        this.error = true;
+        return of('');
+      }),
+      finalize(() => this.loading = false)
+    )
+    .subscribe(html => {
+      if (html) this.headerHtml = this.sanitizer.bypassSecurityTrustHtml(html);
     });
   }
-
-  private reinitializeHeaderScripts() {
-    // If the header uses a standard Bootstrap dropdown or custom jQuery hover:
-    if (typeof $ !== 'undefined') {
-      // Example: Force re-binding of hover/dropdowns
-      $('.dropdown').hover(
-        () => { $(this).addClass('show').find('.dropdown-menu').addClass('show'); },
-        () => { $(this).removeClass('show').find('.dropdown-menu').removeClass('show'); }
-      );
-    }
-    
-    // Check if the remote header requires a specific global function to be called
-    // Many LPU headers use a function like initMenu() or layout.init()
-  }
-
-  ngAfterViewInit(): void {
-    this.loadGTMScript('GTM-P8ZP9K2');
-  }
-
-  private loadGTMScript(gtmId: string): void {
-    if (this.document.getElementById('gtm-js')) return;
-    const script = this.document.createElement('script');
-    script.id = 'gtm-js';
-    script.innerHTML = `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${gtmId}');
-    `;
-    this.document.head.appendChild(script);
-  }
 }
-// import { Component, Inject, OnInit } from '@angular/core';
+
+// import { Component, Inject, OnInit, AfterViewInit } from '@angular/core';
 // import { HttpClient } from '@angular/common/http';
 // import { DOCUMENT } from '@angular/common';
-
 // import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+// // Declare jQuery if the header uses it for hover effects
+// declare var $: any;
+
 // @Component({
 //   selector: 'app-header',
 //   templateUrl: './header.component.html'
 // })
-// export class HeaderComponent implements OnInit {
-//    headerHtml: SafeHtml = '';
+// export class HeaderComponent implements OnInit, AfterViewInit {
+//   headerHtml: SafeHtml = '';
 
 //   constructor(
 //     private http: HttpClient,
@@ -83,26 +63,42 @@ export class HeaderComponent implements OnInit, AfterViewInit {
 //     @Inject(DOCUMENT) private document: Document
 //   ) {}
 
-//   ngOnInit() {
-//     // Load remote header HTML
-//     this.http
-//       .get('https://includepages.lpu.in/newlpu/header.php', { responseType: 'text' })
-//       .subscribe({
-//         next: html => {
-//           this.headerHtml = this.sanitizer.bypassSecurityTrustHtml(html);
-//         },
-//         error: err => {
-//           console.error('Error fetching PHP header:', err);
-//         }
-//       });
+//   ngOnInit(): void {
+//     this.http.get('/php-header', { responseType: 'text' }).subscribe({
+//       next: (html: string) => {
+//         this.headerHtml = this.sanitizer.bypassSecurityTrustHtml(html);
+        
+//         // Wait a tiny bit for Angular to render the HTML into the view
+//         setTimeout(() => {
+//           this.reinitializeHeaderScripts();
+//         }, 100);
+//       },
+//       error: (err) => console.error('Error:', err)
+//     });
 //   }
 
-//   ngAfterViewInit() {
+//   private reinitializeHeaderScripts() {
+//     // If the header uses a standard Bootstrap dropdown or custom jQuery hover:
+//     if (typeof $ !== 'undefined') {
+//       // Example: Force re-binding of hover/dropdowns
+//       $('.dropdown').hover(
+//         () => { $(this).addClass('show').find('.dropdown-menu').addClass('show'); },
+//         () => { $(this).removeClass('show').find('.dropdown-menu').removeClass('show'); }
+//       );
+//     }
+    
+//     // Check if the remote header requires a specific global function to be called
+//     // Many LPU headers use a function like initMenu() or layout.init()
+//   }
+
+//   ngAfterViewInit(): void {
 //     this.loadGTMScript('GTM-P8ZP9K2');
 //   }
 
-//   loadGTMScript(gtmId: string) {
+//   private loadGTMScript(gtmId: string): void {
+//     if (this.document.getElementById('gtm-js')) return;
 //     const script = this.document.createElement('script');
+//     script.id = 'gtm-js';
 //     script.innerHTML = `
 //       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 //       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -113,4 +109,52 @@ export class HeaderComponent implements OnInit, AfterViewInit {
 //     this.document.head.appendChild(script);
 //   }
 // }
+// // import { Component, Inject, OnInit } from '@angular/core';
+// // import { HttpClient } from '@angular/common/http';
+// // import { DOCUMENT } from '@angular/common';
+
+// // import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+// // @Component({
+// //   selector: 'app-header',
+// //   templateUrl: './header.component.html'
+// // })
+// // export class HeaderComponent implements OnInit {
+// //    headerHtml: SafeHtml = '';
+
+// //   constructor(
+// //     private http: HttpClient,
+// //     private sanitizer: DomSanitizer,
+// //     @Inject(DOCUMENT) private document: Document
+// //   ) {}
+
+// //   ngOnInit() {
+// //     // Load remote header HTML
+// //     this.http
+// //       .get('https://includepages.lpu.in/newlpu/header.php', { responseType: 'text' })
+// //       .subscribe({
+// //         next: html => {
+// //           this.headerHtml = this.sanitizer.bypassSecurityTrustHtml(html);
+// //         },
+// //         error: err => {
+// //           console.error('Error fetching PHP header:', err);
+// //         }
+// //       });
+// //   }
+
+// //   ngAfterViewInit() {
+// //     this.loadGTMScript('GTM-P8ZP9K2');
+// //   }
+
+// //   loadGTMScript(gtmId: string) {
+// //     const script = this.document.createElement('script');
+// //     script.innerHTML = `
+// //       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+// //       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+// //       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+// //       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+// //       })(window,document,'script','dataLayer','${gtmId}');
+// //     `;
+// //     this.document.head.appendChild(script);
+// //   }
+// // }
 
